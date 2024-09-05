51cb090fe48de0b6581c46c1bf2a3fb7ec799af9
Irecolichelli <irenecolichelli@gmail.com>
2024-09-05 16:25:47 -0300

    fix: clasification of tags

--- models/project_tags.py
+++ models/project_tags.py
@@ -3,6 +3,6 @@ from odoo import fields, models
 class ProjectTag(models.Model):
     _inherit = 'project.tags'
 
-    importation_tag_import = fields.Boolean(string="Etiqueta de Importación (Importación)")
-   
+    #importation_tag_import = fields.Boolean(string="Etiqueta de Importación (Importación)")
+    is_import = fields.Boolean(string="Importación", default=True)
 
